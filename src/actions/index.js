aab7245ce42916f662aa5b21c5b6335e4975e537
kaisanjay <sanjaykai.1994@gmail.com>
2018-04-26 18:34:52 +0530

    action axios basic

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-export function loadColor({
-	return(dispatch)
-})
+export function loadColor(){
+	return(dispatch)=>{
+		return axios.get("http://www.colr.org/json/color/random").then(response)=>{
+			dispatch(changeColor("#"+response.data.new.color))
+		}
+	}
+}
