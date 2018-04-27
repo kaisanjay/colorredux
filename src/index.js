856588dd9932cbb75cabe6cfeb78a01d55cccb71
kaisanjay <sanjaykai.1994@gmail.com>
2018-04-27 16:25:08 +0530

    thunk setup and import in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducer/index.js';
 import BoxCon from './container/box-container.js'
 import './index.css';
 
